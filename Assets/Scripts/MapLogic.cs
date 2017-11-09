d8f6cb0ce7cbb7691a99494fb62bc3bc82d7482d
Alexander <alx168@gmail.com>
2017-11-09 01:41:52 -0600

    MapArray
    Began MapLogic for Storage of Tiles using a 2D array.

--- Assets/Scripts/MapLogic.cs
+++ Assets/Scripts/MapLogic.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapLogic : MonoBehaviour {
+	[SerializeField] 
+	int rows;
+	[SerializeField]
+	int coloumns;
+	[SerializeField]
+	Sprite tile;
 
+	private SpriteRenderer spriteR;
+
+	//2D array to store the tiles and use them when necessary
+	Sprite[,] mapTiles = new Sprite[rows,coloumns];
 	// Use this for initialization
 	void Start () {
-		
+		//initialize the tile sprite into the 2D array.  Can update for more varied map tiles later
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
+		for (int i = 0; i < rows; i++) {
+			Debug.Log ("testing");
+			for (int j = 0; j < coloumns; j++) {
+				mapTiles [i,j] = tile;
+			}
+		}
+		//mapTiles = Resources.LoadAll<Sprite>(tile);
 	}
 	
 	// Update is called once per frame
