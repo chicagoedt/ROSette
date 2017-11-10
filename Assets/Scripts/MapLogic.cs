509576c1f942adb67f66e2a119002d4769e82bee
Alexander <alx168@gmail.com>
2017-11-10 02:26:54 -0600

    tilmap investigation
    will look into tilemaps to improve the way we display and interact with map.  If tilemaps are only for drawing the map and we can't interact with it then we will stick with this.

--- Assets/Scripts/MapLogic.cs
+++ Assets/Scripts/MapLogic.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapLogic : MonoBehaviour { 
+
+	//Look into tilemaps for unity to replace this quick and dirty system.
+
+
 	//I made a serializefield for this and we might want to consider making rows and coloumns public fields.
 	//Did this because I was interested and can change this later on.  I believe this makes things easier to test.
 	[SerializeField]
@@ -14,20 +18,20 @@ public class MapLogic : MonoBehaviour {
 	private Sprite tile;
 
 	private Sprite[,] mapTiles;
-	private SpriteRenderer spriteR;
+	public SpriteRenderer spriteR;
 
 	// Use this for initialization
 	void Start () {
+		//initialize the sprite renderer
+		spriteR = gameObject.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
 		//2D array to store the tiles and use them when necessary
 		Sprite[,] mapTiles = new Sprite[rows,columns];
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				mapTiles [i,j] = tile;
+
 			}
 		}
-		//initialize the tile sprite into the 2D array.  Can update for more varied map tiles later
-		spriteR = gameObject.GetComponent<SpriteRenderer>();
-		//mapTiles = Resources.LoadAll<Sprite>(tile);
 	}
 	
 	// Update is called once per frame
