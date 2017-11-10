fc404b006cd5f699a2b60f49ae2e2176163b3aec
austin-ek <mraustinek@gmail.com>
2017-11-10 12:59:44 -0600

    added block lookup system

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEditor;
+
+public class Panel {
+
+    //Will contain all of the blocks in this panel, in order
+    ArrayList blocks;
+
+    public Panel(){
+        blocks = new ArrayList();
+
+    }
+
+    //To add a block to this panel
+    void insertBlock(Block block){
+        blocks.Add(block);
+    }
+
+    void insertBlock(Block block, int pos){
+        try{
+            blocks.Insert(pos, block);
+        }
+        catch(System.IndexOutOfRangeException e){
+            UnityEngine.Debug.Log(e.StackTrace);
+        }
+    }
+
+    void removeBlock(int pos){
+        try{
+            blocks.RemoveAt(pos);
+        }
+        catch (System.IndexOutOfRangeException e){
+            UnityEngine.Debug.Log(e.StackTrace);
+        }
+    }
+
+    class Block{
+        int blockID;
+        object value;
+
+        public Block(int id){
+            this.blockID = id;
+        }
+
+        void setValue(object val){
+            this.value = val;
+        }
+
+        object getValue(){
+            return value;
+        }
+    }
+}
---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEditor;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+[InitializeOnLoad]
+public class PanelManager : MonoBehaviour {
+    ArrayList panels;
+
+    static PanelManager()
+    {
+        UnityEngine.Debug.Log(lookupBlockByID(0));
+    }
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    static string lookupBlockByID(int id){
+        string name = "";
+        
+        string conn = "URI=file:" + Application.dataPath + "/Blocks.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+        string sqlQuery = "SELECT Name " + "FROM Blocks " + "WHERE ID = " + id.ToString();
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        if (reader.Read())
+        {
+            name = reader.GetString(0);
+        }
+        else return null; 
+
+        reader.Close();
+        dbcmd.Dispose();
+        dbconn.Close();
+
+        return name;
+    }
+}
