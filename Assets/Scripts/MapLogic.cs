b7d4b88b9c929e1a1ac60d37d943dcb04449c7a9
Alexander <alx168@gmail.com>
2017-11-10 02:08:04 -0600

    Fixed Array
    Array works now.  Just need to have the sprites render.

--- Assets/Scripts/MapLogic.cs
+++ Assets/Scripts/MapLogic.cs
@@ -1,34 +1,37 @@
-﻿using System.Collections;
+﻿using System.Runtime.CompilerServices;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapLogic : MonoBehaviour {
-	[SerializeField] 
-	int rows;
+public class MapLogic : MonoBehaviour { 
+	//I made a serializefield for this and we might want to consider making rows and coloumns public fields.
+	//Did this because I was interested and can change this later on.  I believe this makes things easier to test.
 	[SerializeField]
-	int coloumns;
+	private int rows;
 	[SerializeField]
-	Sprite tile;
+	private int columns;
+	[SerializeField]
+	private Sprite tile;
 
+	private Sprite[,] mapTiles;
 	private SpriteRenderer spriteR;
 
-	//2D array to store the tiles and use them when necessary
-	Sprite[,] mapTiles = new Sprite[rows,coloumns];
 	// Use this for initialization
 	void Start () {
-		//initialize the tile sprite into the 2D array.  Can update for more varied map tiles later
-		spriteR = gameObject.GetComponent<SpriteRenderer>();
+		//2D array to store the tiles and use them when necessary
+		Sprite[,] mapTiles = new Sprite[rows,columns];
 		for (int i = 0; i < rows; i++) {
-			Debug.Log ("testing");
-			for (int j = 0; j < coloumns; j++) {
+			for (int j = 0; j < columns; j++) {
 				mapTiles [i,j] = tile;
 			}
 		}
+		//initialize the tile sprite into the 2D array.  Can update for more varied map tiles later
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
 		//mapTiles = Resources.LoadAll<Sprite>(tile);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
